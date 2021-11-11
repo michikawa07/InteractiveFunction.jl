a47574264d7d96ee80104cf82b1df7e858482ffa
michikawa07 <michikawa.ryohei@gmail.com>
2021-11-11 12:10:56 +0900

    update
    Become to present the files already watched by Revise
    
    Rename menu functions

--- src/InteractiveFunctions.jl
+++ src/InteractiveFunctions.jl
@@ -1,22 +1,85 @@
 module InteractiveFunctions
-	using Lazy
 	using Revise
-	using REPL.TerminalMenus:RadioMenu,MultiSelectMenu,request,config
+	using REPL.TerminalMenus
+	using InteractiveUtils
 
-	export includet_menu, cd_menu
+	export includemt, cdm
 
-	function readdir_all(dir=".")
+	"""
+		dir 以下のファイルをすべて参照し，dirとの相対Pathを返す．
+	"""
+	function readdirs(dir=pwd(); join=false)
 		filecontents = String[]
-		for (root, dirs, files) in walkdir(dir), file in files
-			path = @> joinpath(root, file) replace("\\"=>"/") replace("./"=>"")
-			push!(filecontents,path) # path to files
+		for (root, dirs, filenames) in walkdir(dir)
+			#///folder = relpath( root, dir )
+			#///paths = joinpath.(folder, filenames )
+			paths_full = joinpath.(root, filenames )
+			paths_rel = relpath.(paths_full , dir )
+			push!(filecontents,paths_rel...) # path to files
 		end
 		filecontents
 	end	
 
-	function includet_menu()
+	function getRevisedFileName(dir=pwd())
+		revisedfile = String[]
+		for (root,files) in Revise.watched_files
+			filenames = keys(files.trackedfiles)
+			#///folder = relpath( root, dir )
+			#///paths = joinpath.(folder, filenames)
+			paths_full = joinpath.(root, filenames )
+			paths_rel = relpath.(paths_full , dir )
+			push!(revisedfile,paths_rel...)
+		end
+		revisedfile
+	end
+
+	function includemt(;showFunc=true,showVar=false)
 		try	
-			list = readdir_all() |> l->l[occursin.(r".jl$",l)]
+			list = readdirs() |> l->l[occursin.(r".jl$",l)]
+			selectedlist = getRevisedFileName() |> l->l[.!occursin.("..",l)]
+			selected = [i for (i,l) in enumerate(list) if l ∈ selectedlist]
+
+			TerminalMenus.config(ctrl_c_interrupt = false)
+			menu = MultiSelectMenu(list;selected)
+			choice = request("\n== choice revising file ==", menu)
+			println("==========================")
+			
+			#=if=# length(choice) ≤ 0 && throw("cancel")
+			for file in list[choice|>collect]
+				file ∈ selectedlist && continue
+				showFunc && println("\n includet( \"$(file)\" )")
+				stats = @timed includet(pwd()*"/".*file)
+				println("  - success (time:$(stats.time))\n")
+			end
+		catch e ;println("\n - $e\n")
+		end
+		
+		showVar || return
+		println("== Variables and Functions ==\n")
+		varinfo() |> display
+	end
+
+	function cdm()
+		try	
+			list = readdir() |> l->["..", l[isdir.(l)]...]
+			TerminalMenus.config(ctrl_c_interrupt = false)
+			menu = RadioMenu(list.*"/")
+			choice = request("\n== choice changing directory ==", menu)
+
+			#=if=# choice == -1 && throw("cancel")
+			println("==============================\n")
+			println(" cd( \"$(list[choice])\" )")
+			cd(list[choice])
+			println("  - success")
+		catch e ;println("\n - $e\n")
+		end
+	end
+
+	macro multiSelectMenud(f, title)
+		#f_d = Symbol(f,"_menued")
+		#@eval function $f_d(x...; y...) $f($x, x...;y...) end
+		try	
+			list = readdirs() |> l->l[occursin.(r".jl$",l)]
 			config(ctrl_c_interrupt = false)
 			menu = MultiSelectMenu(list)
 			choice = request("\n== choice revising file ==", menu)
@@ -29,21 +92,5 @@ module InteractiveFunctions
 			end
 		catch e ;println("\n - $e\n")
 		end
-	end
-
-	function cd_menu()
-		try	
-			list = readdir() |> l->["..", l[isdir.(l)]...]
-			config(ctrl_c_interrupt = false)
-			menu = RadioMenu(list.*"/")
-			choice = request("\n~~ choice changing directory ~~", menu)
-			#=if=# choice == -1 && throw("cancel")
-			println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
-			println(" cd( \"$(list[choice])\" )")
-			cd(list[choice])
-			println("  - success")
-		catch e ;println("\n - $e\n")
-		end
-	end
-#[(v.trackedfiles|>keys)  for v in Revise.watched_files |> values]
+	end	
 end

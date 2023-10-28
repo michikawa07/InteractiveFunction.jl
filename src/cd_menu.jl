1cfba25d2854f43af3fe0e1cbff248e6d862f433
michikawa07 <michikawa.ryohei@gmail.com>
2023-10-28 18:41:15 +0900

    update @cd

--- src/cd_menu.jl
+++ src/cd_menu.jl
@@ -2,47 +2,60 @@ using REPL.TerminalMenus
 
 export cd_menu, @cd, CD
 
-function clean(H)
-	buf = IOBuffer()
-	for _ in 1:H+1
-		 print(buf, "\x1b[2K") # clear line
-		 print(buf, "\x1b[999D\x1b[$(1)A") # rollback
+""" cd() の簡単なラッパー """
+function cd_menu(show_files=true)
+	repl_h, repl_w = displaysize(stdout)
+	max_w = repl_w - 12
+	H_dir, H_file = 3, 0
+	current_path = "~\\$(relpath( pwd(), homedir()))\\"
+	if length(current_path) > max_w
+		current_path = "..." * current_path[end-max_w:end]
 	end
-	print(buf |> take! |> String)
-end
-
-function cd_menu(;karg=false)
-	header = "\n== choice changing directory =="
-	footer =   "==============================="
+	separater = " - - - - - - - - - - - - - - - "
+	footer =    "==============================="
 	try
-		list = filter( isdir, readdir() )
-		pushfirst!(list, "..")
-		menu =  RadioMenu(list.*"/")
+		dirs  = [ ".."; filter( isdir, readdir() )]
+		files = filter( isfile, readdir() )
+		menu =  RadioMenu(dirs.*"/")
 		size = menu.pagesize+menu.pageoffset
 
-		H = 3+min(length(list), size)
+		H_dir  += min(length(dirs), size)
+		H_file += show_files ? length(files) : 0
 		buf = IOBuffer()
-		print(buf, "\n"^(H))
+		print(buf, "\n"^(H_dir+H_file))
 		print(buf, footer)
-		print(buf, "\x1b[999D\x1b[$(H-1)A") # rollback
+		show_files && begin
+			foreach(files|>reverse) do f
+				print(buf, "\x1b[999D\x1b[1A")
+				print(buf, "   $f")
+			end
+			print(buf, "\x1b[999D\x1b[1A")
+			print(buf, separater)
+		end
+		print(buf, "\x1b[999D\x1b[$(H_dir-2)A") # rollback
 		print(buf |> take! |> String)
-		
-		@info " ~\\$(relpath( pwd(), homedir())) \x1b[999D\x1b[$(1)A"
-		
-		choice = request(header, menu)
-		choice == -1 && return println()
 
-		cd( list[choice] )
+		@info "$current_path"
+		choice = request(menu)
+		choice == -1 && return throw(InterruptException())
 
-		clean( H-1 )
-		cd_menu()
+		cd( dirs[choice] )
+
+		buf = IOBuffer()
+		print(buf, "\x1b[999D\x1b[$(H_file+1)B")
+		for _ in 1:H_dir+H_file
+			 print(buf, "\x1b[2K") # clear line
+			 print(buf, "\x1b[999D\x1b[1A") # rollback
+		end
+		print(buf |> take! |> String)
+		cd_menu(show_files)
 	catch e 
-		e == InterruptException() && return println("\n\n - cancel")
+		e == InterruptException() && return print("$("\n"^H_file)\n\n")
 		#= other =# throw(e)
 	end
 end
 
 const CD = cd_menu
-macro cd(karg...)
-	:(cd_menu($karg...))
+macro cd(arg...)
+	:(cd_menu($arg...))
 end
